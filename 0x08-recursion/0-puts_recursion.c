d91ce417d9c3c4fc7c09efeab077132227554e6c
TheYamikani <theyamikani@gmail.com>
2023-04-05 19:08:56 -0700

    hello

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: points to char array or string
+ * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
-else
-putchar('\n');
+	unsigned int index = 0;
+
+	if (s[index] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[index]);
+	_puts_recursion(s + 1);
 }
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H_FILE
+#define HOLBERTON_H_FILE
 
 int _putchar(char c);
 void _puts_recursion(char *s);
